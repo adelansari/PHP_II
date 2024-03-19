8ce25a65679ffef983de9a8a2d2585131512a3fe
Adel Ansari <adelansari.a@gmail.com>
2024-03-19 14:35:30 +0200

    Update variable values and display in HTML

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/updating-variables.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/updating-variables.php
@@ -1,9 +1,17 @@
 <?php
 
 /* 
-  Write you php code here
+  Write a PHP code to update a value in a variable so that the cost of candy per pack is shown and the initial name variable is changed to something else. For example, if initial name is “Guest”, you can update it to “Your Name”
+*/
 
-   */
+$name = "Guest";
+$price = 1.50;
+
+if ($name == "Guest") {
+  $name = "Adel";
+  $price = 2.00;
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -16,6 +24,8 @@
 
 <body>
   <h1>The Candy Store</h1>
+  <h2>Welcome <?php echo $name; ?>!</h2>
+  <p>The cost of the candy is <?php echo $price; ?> per pack.</p>
 
 </body>
 

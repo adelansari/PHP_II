a09749df362b54655624e3029b3f6bf3521fd5cd
Adel Ansari <adelansari.a@gmail.com>
2024-03-19 14:42:45 +0200

    Add multidimensional array for storing offers and display them on the webpage

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/multidimensional-arrays.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/multidimensional-arrays.php
@@ -1,9 +1,26 @@
 <?php
 
 /* 
-  Write you php code here
+  Write a PHP code to store indexed arrays in an array or multidimentional array with variable called $offers. Each element in the array stores associated array holding name, price, and stock level of an item that is on offer. Print out the name,price and stock of all the products.
+*/
 
-   */
+$offers = array(
+  array(
+    "name" => "Lollipop",
+    "price" => 0.3,
+    "stock" => 100
+  ),
+  array(
+    "name" => "Toffee",
+    "price" => 0.4,
+    "stock" => 50
+  ),
+  array(
+    "name" => "Candy Cane",
+    "price" => 0.5,
+    "stock" => 30
+  )
+);
 
 ?>
 <!DOCTYPE html>
@@ -17,6 +34,11 @@
 <body>
   <h1>The Candy Store</h1>
   <h2>Offers</h2>
+  <?php
+  foreach ($offers as $offer) {
+    echo "<p>" . $offer["name"] . " - €" . $offer["price"] . " - " . $offer["stock"] . " in stock</p>";
+  }
+  ?>
 
 </body>
 

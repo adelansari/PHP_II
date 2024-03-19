0885bc872ec95bfa32cb8f01798a833cf4698bff
Adel Ansari <adelansari.a@gmail.com>
2024-03-19 14:42:26 +0200

    Update and add elements to nutrition array

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/updating-arrays.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/updating-arrays.php
@@ -1,9 +1,17 @@
 <?php
 
 /* 
-  Write you php code here
+  Write a PHP code to  create and store array in $nutrition, the value that is stored for the fat content should be updated and add a new element e.g. fiber and write those values out to the page.
+*/
 
-   */
+$nutrition = array(
+  "fat" => "0.2g",
+  "sugar" => "14g",
+  "salt" => "0.01g"
+);
+
+$nutrition["fat"] = "0.5g";
+$nutrition["fiber"] = "1.2g";
 
 ?>
 <!DOCTYPE html>
@@ -17,6 +25,11 @@
 <body>
   <h1>The Candy Store</h1>
   <h2>Nutrition (per 100g)</h2>
+  <?php
+  foreach ($nutrition as $key => $value) {
+    echo "<p>" . $key . ": " . $value . "</p>";
+  }
+  ?>
 
 </body>
 

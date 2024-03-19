0f5184bfcc8a8c5f99ee4782719d4acb92921541
Adel Ansari <adelansari.a@gmail.com>
2024-03-19 14:35:39 +0200

    Add associative array for nutrition and display percentages

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/associative-arrays.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/associative-arrays.php
@@ -1,9 +1,14 @@
 <?php
 
 /* 
-  Write you php code here
+  Write a PHP code to  create and store array in a  variable called $nutrition with fat, sugar and salt and display the contents of Nutrition (per 100G) in percentage
+*/
 
-   */
+$nutrition = array(
+  "fat" => 0.2,
+  "sugar" => 0.5,
+  "salt" => 0.1
+);
 
 ?>
 <!DOCTYPE html>
@@ -17,6 +22,9 @@
 <body>
   <h1>The Candy Store</h1>
   <h2>Nutrition (per 100g)</h2>
+  <p>Fat: <?php echo $nutrition["fat"] * 100; ?>%</p>
+  <p>Sugar: <?php echo $nutrition["sugar"] * 100; ?>%</p>
+  <p>Salt: <?php echo $nutrition["salt"] * 100; ?>%</p>
 
 </body>
 

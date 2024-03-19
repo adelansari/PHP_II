800dd968329af82f6ef02a9c0f95f5dcee521045
Adel Ansari <adelansari.a@gmail.com>
2024-03-19 14:35:46 +0200

    Add code to display only three best-selling items

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/indexed-arrays.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/indexed-arrays.php
@@ -1,9 +1,18 @@
 <?php
 
 /* 
-  Write you php code here
+  Write a PHP code to  create and store array for $best_sellers where it holds list of best-selling items  but display only three best-selling items on the page. The list of best-sellers could be e.g. Chocolate, Mints, Fudge, Bubble gum, Toffee, Jelly Beans etc
+*/
 
-   */
+$best_sellers = array(
+  "Chocolate",
+  "Mints",
+  "Fudge",
+  "Bubble gum",
+  "Toffee",
+  "Jelly Beans"
+);
+
 
 ?>
 <!DOCTYPE html>
@@ -17,6 +26,17 @@
 <body>
   <h1>The Candy Store</h1>
   <h2>Best Sellers</h2>
+  <?php
+  foreach ($best_sellers as $key => $value) {
+    if ($key < 3) {
+      echo "<p>" . ($key + 1) . ". " . $value . "</p>";
+    }
+  }
+  ?>
+
+
+
+
 
 </body>
 

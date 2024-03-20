c8896c8548e8402fc884c4cbc7a5c667d2203577
Adel Ansari <adelansari.a@gmail.com>
2024-03-20 11:28:16 +0200

    Add if-else-if statement to check candy stock status

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/if-else-if-statement.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/if-else-if-statement.php
@@ -1,5 +1,10 @@
 <?php
-/* Write your code here */
+/*
+  Create a simple if else if statement to check if candy is in the stock or is coming soon or sold out. If candy is not in stock then print message “Sold Out”, if it is available in stock, print message “In Stock”
+*/
+$candy = "Chocolate";
+$stock = "In Stock";
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +18,16 @@
   <h1>The Candy Store</h1>
   <h2>Chocolate</h2>
   <?php
-  /* Write your code here */
+  if ($stock == "In Stock") {
+    echo "<p>\"$candy\" is $stock</p>";
+  } elseif ($stock == "Coming Soon") {
+    echo "<p>\"$candy\" is $stock</p>";
+  } else {
+    echo "<p>\"$candy\" is Sold Out</p>";
+  }
+
+  // alternative method
+  echo "<p>\"$candy\" is " . ($stock == "In Stock" ? $stock : ($stock == "Coming Soon" ? $stock : "Sold Out")) . "</p>";
   ?>
 </body>
 

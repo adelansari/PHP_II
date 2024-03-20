709b14ba9059d46708ddfe6609e058fa2dcb830d
Adel Ansari <adelansari.a@gmail.com>
2024-03-20 14:29:22 +0200

    Add switch statement for candy discounts

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/switch-statement.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/switch-statement.php
@@ -1,5 +1,29 @@
 <?php
-/* Write your code here */
+/*
+  Create a simple switch statement to get 20% off chocolates on Monday and 20% off mints on Tuesday, and in all other cases, it should show “Buy three packs, get one free.
+*/
+$discounts = array(
+  "Monday" => "20% off chocolates",
+  "Tuesday" => "20% off mints",
+  "Other days" => "Buy three packs, get one free"
+);
+
+$candy = "chocolate";
+$day = "Monday";
+
+switch ($day) {
+  case "Monday":
+  case "Tuesday":
+    $discount = $discounts[$day];
+    break;
+  default:
+    $discount = $discounts["Other days"];
+    break;
+}
+
+// OR
+// $discount = isset($discounts[$day]) ? $discounts[$day] : $discounts["Other days"];
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,8 +35,18 @@
 
 <body>
   <h1>The Candy Store</h1>
+  <h2>Chocolate Discount:</h2>
+  <h2>Monday: <?php echo $discounts["Monday"]; ?></h2>
+  <h2>Tuesday: <?php echo $discounts["Tuesday"]; ?></h2>
+  <h2>Rest of the days: <?php echo $discounts["Other days"]; ?></h2>
+
+  <?php
+  echo "<p>Today's offer for \"$candy\" is: $discount</p>";
+  ?>
+
   <?php
   /* Write your code here */
+  echo "<p>\"$candy\" is $discount</p>";
   ?>
 </body>
 

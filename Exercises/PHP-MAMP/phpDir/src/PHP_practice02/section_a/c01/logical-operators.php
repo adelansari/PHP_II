83916e0c920a65e2fcd054bae0f7a677896a799f
Adel Ansari <adelansari.a@gmail.com>
2024-03-20 10:32:39 +0200

    Add logic to check if customer can buy limited packs of candy

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/logical-operators.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice02/section_a/c01/logical-operators.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+/*
+  Write a PHP code to check if the customer only wants to buy limited packs of candy. Check if there are enough items in stock and secondly check that the item can be delivered. You can put imaginery number to do the comparision. 
+*/
 $item    = 'Chocolate';
 $stock   = 5;
 $wanted  = 3;
@@ -7,16 +10,19 @@ $can_buy = (($wanted <= $stock) && ($deliver == true));
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
-  <head>
-    <title>Logical Operators</title>
-    <link rel="stylesheet" href="css/styles.css">
-  </head>
-  <body>
-    <h1>The Candy Store</h1>
-    <h1>Shopping Cart</h1>
-    <p>Item:    <?= $item ?></p>
-    <p>Stock:   <?= $stock ?></p>
-    <p>Ordered: <?= $wanted ?></p>
-    <p>Can buy: <?= $can_buy ?></p>
-  </body>
+
+<head>
+  <title>Logical Operators</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <h1>The Candy Store</h1>
+  <h1>Shopping Cart</h1>
+  <p>Item: <?= $item ?></p>
+  <p>Stock: <?= $stock ?></p>
+  <p>Ordered: <?= $wanted ?></p>
+  <p>Can buy: <?= $can_buy ?></p>
+</body>
+
 </html>

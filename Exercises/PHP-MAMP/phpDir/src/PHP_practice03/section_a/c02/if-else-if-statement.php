c6bfc2fdccfe85f64654d0bab041fe02d428274a
Adel Ansari <adelansari.a@gmail.com>
2024-03-20 14:29:05 +0200

    Refactored if-else-if statements in if-else-if-statement.php

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/if-else-if-statement.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/if-else-if-statement.php
@@ -17,17 +17,32 @@ $stock = "In Stock";
 <body>
   <h1>The Candy Store</h1>
   <h2>Chocolate</h2>
-  <?php
-  if ($stock == "In Stock") {
-    echo "<p>\"$candy\" is $stock</p>";
-  } elseif ($stock == "Coming Soon") {
-    echo "<p>\"$candy\" is $stock</p>";
-  } else {
-    echo "<p>\"$candy\" is Sold Out</p>";
-  }
 
-  // alternative method
-  echo "<p>\"$candy\" is " . ($stock == "In Stock" ? $stock : ($stock == "Coming Soon" ? $stock : "Sold Out")) . "</p>";
+  <!-- Method A -->
+  <h2>Method A</h2>
+  <?php if ($stock == "In Stock" || $stock == "Coming Soon") : ?>
+    <p><?= "\"$candy\" is $stock"; ?></p>
+
+  <?php else : ?>
+    <p><?= "\"$candy\" is Sold Out"; ?></p>
+
+  <?php endif; ?>
+
+
+  <!-- Method B -->
+  <h2>Method B</h2>
+  <?php
+  $message = "\"$candy\" is ";
+  $message .= ($stock == "In Stock" || $stock == "Coming Soon") ? $stock : "Sold Out";
+  ?>
+  <p><?= $message; ?></p>
+
+
+  <!-- Method C -->
+  <h2>Method C</h2>
+  <?php
+  echo "
+  <p>\"$candy\" is " . ($stock == "In Stock" ? $stock : ($stock == "Coming Soon" ? $stock : "Sold Out")) . "</p>";
   ?>
 </body>
 

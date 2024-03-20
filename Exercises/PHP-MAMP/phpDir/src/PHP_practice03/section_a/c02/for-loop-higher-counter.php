a4d35af6a2d5ccc9f2ca393c8ff36c10e3b52f1d
Adel Ansari <adelansari.a@gmail.com>
2024-03-20 14:28:46 +0200

    Add for loop to calculate prices for multiple packs of candy

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/for-loop-higher-counter.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice03/section_a/c02/for-loop-higher-counter.php
@@ -1,5 +1,13 @@
 <?php
-/* Write your code here */
+/*
+  Create a simple for loop to find prices of multiple higher packs of candy. Let us assume the customer wants 10 packs to 100 packs of candies. How much do 10 to 100 packs cost? Display from 10 packs to 100 packs cost on the web page.
+*/
+
+$price = 0.25;
+$startPack = 10;
+$endPack = 100;
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,9 +20,19 @@
 <body>
   <h1>The Candy Store</h1>
   <h2>Prices for Large Orders</h2>
+  <h2>Method A</h2>
+  <?php for ($startPack; $startPack <= $endPack; $startPack++) : ?>
+    <p>Price for <?php echo $startPack; ?> pack(s): $<?php echo number_format($price * $startPack, 2); ?></p>
+  <?php endfor; ?>
+
+  <!-- Alternative way: -->
+  <h2>Method B</h2>
   <p>
     <?php
     /* Write your code here */
+    for ($startPack; $startPack <= $endPack; $startPack++) {
+      echo "<p>Price for $startPack pack(s): $" . number_format($price * $startPack, 2) . "</p>";
+    }
     ?>
   </p>
 </body>

3448185a7018eab47dbc47fe13b004841f4bb3fc
Adel Ansari <adelansari.a@gmail.com>
2024-03-21 11:28:43 +0200

    Add array updating functions to section_b/c05

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice05/section_b/c05/array-updating-functions.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice05/section_b/c05/array-updating-functions.php
@@ -1,11 +1,22 @@
 <?php
-// Write your code here
-
+/*
+    Write PHP Code to 
+    - create array of items (items could be e.g. “notebook”, “pencil”, “eraser”) being ordered
+    - add a new item (i.e. “scissors”) to start of an array
+    - remove the last item from array
+*/
+$items = ["notebook", "pencil", "eraser"];
+array_unshift($items, "scissors");
+array_pop($items);
 ?>
 <?php include 'includes/header.php'; ?>
 
 <h1>Order</h1>
 
-// Write your code here
+<ul>
+    <?php foreach ($items as $item) : ?>
+        <li><?= $item; ?></li>
+    <?php endforeach; ?>
+</ul>
 
 <?php include 'includes/footer.php'; ?>

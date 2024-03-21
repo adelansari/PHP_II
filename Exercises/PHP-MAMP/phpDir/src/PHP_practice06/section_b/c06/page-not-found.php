b16399cfaf3c01bc19f229d27a418dffb479e797
Adel Ansari <adelansari.a@gmail.com>
2024-03-21 13:48:47 +0200

    Refactor page-not-found.php

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice06/section_b/c06/page-not-found.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice06/section_b/c06/page-not-found.php
@@ -1,23 +1,26 @@
 <?php
-  $cities  = ['London' => '12 Albermarle Street, N2 1DH',
-              'Sydney' => '100 Oxford Street, 2021',
-              'NYC'    => '1242 7th Street, 10492',];
+$cities  = [
+  'London' => '12 Albermarle Street, N2 1DH',
+  'Sydney' => '100 Oxford Street, 2021',
+  'NYC'    => '1242 7th Street, 10492',
+];
 
-  $city    = $_GET['city'] ?? '';
+$city    = $_GET['city'] ?? '';
 
-  $valid   = array_key_exists($city, $cities);
+$valid   = array_key_exists($city, $cities);
 
-  if ($valid) {
-  	$address = $cities[$city];
-  } else {
-  	$city    = 'Please select a location';
-  	$address = '';
-  }
+if ($valid) {
+  $address = $cities[$city];
+} else {
+  $city    = 'Please select a location';
+  $address = '';
+}
 ?>
 
 <?php include 'includes/header.php' ?>
 
 <h1>Page not found</h1>
 <p>Sorry, we could not find the page you were looking for.</p>
+<a href="get-3.php">Go Back</a>
 
 <?php include 'includes/footer.php' ?>

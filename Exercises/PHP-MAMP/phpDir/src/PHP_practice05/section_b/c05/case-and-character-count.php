def761b6eea97d72661278e86bd5916d49ef3ff8
Adel Ansari <adelansari.a@gmail.com>
2024-03-21 11:28:48 +0200

    Add case and character count functionality

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice05/section_b/c05/case-and-character-count.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice05/section_b/c05/case-and-character-count.php
@@ -1,10 +1,14 @@
 <?php
+/*
+  Write PHP Code to convert case in lowercase, uppercase, count number of characters and word count
+*/
 $text = 'Home sweet home';
 ?>
 <?php include 'includes/header.php'; ?>
 
-<p>
-  /** Write your code here */
-</p>
+<p>Original Text: <?= $text; ?></p>
+<p>Lowercase: <?= strtolower($text); ?></p>
+<p>Uppercase: <?= strtoupper($text); ?></p>
+<p>Character Count: <?= strlen($text); ?></p>
 
 <?php include 'includes/footer.php'; ?>

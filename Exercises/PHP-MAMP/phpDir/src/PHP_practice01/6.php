bc559186506ca8cadcb8da275da6fd26ab29362c
Adel Ansari <adelansari.a@gmail.com>
2024-03-21 13:47:54 +0200

    Add form to submit username to POST super global

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice01/6.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice01/6.php
@@ -13,21 +13,21 @@
 
   <article class="main-content col-xs-8">
 
-
     <?php
-
     /*
-        Step1: Make a form that submits one value to POST super global
-      */
-
+      Step1: Make a form that submits one value to POST super global
+    */
     if (isset($_POST['submit'])) {
-      echo $_POST['name'];
+      $username = $_POST['username'] ?? '';
+      echo $username;
     }
-
-
-
-
     ?>
+
+
+    <form action="6.php" method="post">
+      <input type="text" name="username" placeholder="Enter Username">
+      <input type="submit" name="submit">
+    </form>
 
 
   </article>

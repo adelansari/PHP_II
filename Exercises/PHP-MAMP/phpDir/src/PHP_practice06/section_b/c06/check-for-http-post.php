76b3dcc8c826a8f638fb7cfcccc2d15e9991ce26
Adel Ansari <adelansari.a@gmail.com>
2024-03-21 13:48:21 +0200

    Add HTML form for user input

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice06/section_b/c06/check-for-http-post.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice06/section_b/c06/check-for-http-post.php
@@ -14,9 +14,31 @@ and a message should be displayed like this:
 
 Step 3: Otherwise, simply display the form
 
-
-
 */
+
 ?>
+
+<form method="post">
+  <p>Name: <input type="text" name="name"></p>
+  <p>Age: <input type="text" name="age"></p>
+  <p>Email: <input type="text" name="email"></p>
+  <p>Password: <input type="password" name="pwd"></p>
+  <p>Bio: <textarea name="bio"></textarea></p>
+  <p>Contact preference:
+    <select name="preferences">
+      <option value="email">Email</option>
+      <option value="phone">Phone</option>
+    </select>
+  </p>
+  <p>Rating:
+    1 <input type="radio" name="rating" value="1">
+    2 <input type="radio" name="rating" value="2">
+    3 <input type="radio" name="rating" value="3"></p>
+  <p><input type="checkbox" name="terms" value="true">
+    I agree to the terms and conditions.</p>
+  <p><input type="submit" value="Save"></p>
+</form>
+
+
 
 <?php include 'includes/footer.php'; ?>

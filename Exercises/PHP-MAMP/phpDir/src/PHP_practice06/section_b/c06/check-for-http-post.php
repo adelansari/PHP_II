1dac63f36671719393f60b57932531725cd521b5
Adel Ansari <adelansari.a@gmail.com>
2024-03-23 16:20:59 +0200

    Refactor form handling in check-for-http-post.php

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice06/section_b/c06/check-for-http-post.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice06/section_b/c06/check-for-http-post.php
@@ -1,44 +1,24 @@
 <?php include 'includes/header.php'; ?>
-
 <?php
 /* Write PHP Code here  
 Overall idea here is to check if a form has been submitted
-
-
 Step 1: Use if statement to check $_SERVER superglobal array to see if the key called
 REQUEST_METHOD has a value of POST
-
 Step 2: If it does, the search form has to be sent via HTTP POST, 
 and a message should be displayed like this:
   "You searched for ..."  (replace ... with term user searched for)
-
 Step 3: Otherwise, simply display the form
-
 */
-
 ?>
 
-<form method="post">
-  <p>Name: <input type="text" name="name"></p>
-  <p>Age: <input type="text" name="age"></p>
-  <p>Email: <input type="text" name="email"></p>
-  <p>Password: <input type="password" name="pwd"></p>
-  <p>Bio: <textarea name="bio"></textarea></p>
-  <p>Contact preference:
-    <select name="preferences">
-      <option value="email">Email</option>
-      <option value="phone">Phone</option>
-    </select>
-  </p>
-  <p>Rating:
-    1 <input type="radio" name="rating" value="1">
-    2 <input type="radio" name="rating" value="2">
-    3 <input type="radio" name="rating" value="3"></p>
-  <p><input type="checkbox" name="terms" value="true">
-    I agree to the terms and conditions.</p>
-  <p><input type="submit" value="Save"></p>
-</form>
 
-
-
-<?php include 'includes/footer.php'; ?>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+  $searchTerm = $_POST['search'] ?? ''; ?>
+  <p>You searched for <?= htmlspecialchars($searchTerm) ?></p>
+  <input onclick="window.history.back()" type="submit" value="Go back">
+<?php else : ?>
+  <form method="post">
+    <p>Search: <input type="text" name="search"></p>
+    <p><input type="submit" value="Search"></p>
+  </form>
+<?php endif; ?>

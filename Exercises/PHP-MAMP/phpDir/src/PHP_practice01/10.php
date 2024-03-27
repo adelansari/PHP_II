df5cc8b5dfeddfbda70b423710cc8cb665b8c08f
Adel Ansari <adelansari.a@gmail.com>
2024-03-27 10:42:36 +0200

    Add Dog class with properties and ShowAll method

--- Exercises/PHP-MAMP/phpDir/src/PHP_practice01/10.php
+++ Exercises/PHP-MAMP/phpDir/src/PHP_practice01/10.php
@@ -15,22 +15,37 @@
 
     <?php
 
-		/*  Step 1: Use the Make a class called Dog
-
+    /*  
+    Step 1: Use the Make a class called Dog
 		Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
-
 		Step 4: Make a method named ShowAll that echos all the properties
-
 		Step 5: Instantiate the class / create object and call it pitbull
+    Step 6: Call the method ShowAll
+	  */
 
-Step 6: Call the method ShowAll
+    class Dog
+    {
+      public $eyeColor;
+      public $nose;
+      public $furColor;
 
-	
+      public function __construct($eyeColor, $nose, $furColor)
+      {
+        $this->eyeColor = $eyeColor;
+        $this->nose = $nose;
+        $this->furColor = $furColor;
+      }
 
-		
-	*/
+      public function ShowAll()
+      {
+        echo "Eye Color: $this->eyeColor, Nose: $this->nose, Fur Color: $this->furColor";
+      }
+    }
 
-		?>
+    $pitbull = new Dog('Brown', 'Wet', 'Black');
+    $pitbull->ShowAll();
+
+    ?>
 
 
 
